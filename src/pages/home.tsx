26029ac62dd84327126f5b4d8ee43c2fecfc5c14
Shubh Bhakuni <shubhbhakuni@gmail.com>
2024-10-10 07:37:44 +0530

    Home UI and ProductCard

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,32 @@
-
+import {Link} from 'react-router-dom';
+import ProductCard from '../components/product-card';
 
 const Home = () => {
+
+  const addToCartHandler = () => {};
   return (
-    <div>home</div>
-  )
-}
+    <div className ="home">
+      <section> </section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className = "findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard 
+          productId="asdfgh" 
+          name="Macbook" 
+          price={40000} 
+          stock={435} 
+          handler = {addToCartHandler}
+          photo ="https://m.media-amazon.com/images/I/41DeT328kPL._AC_SY450_.jpg"
+        />
+      </main>
+    </div>
+  );
+};
 
 export default Home
